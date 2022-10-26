fb88f8027e1545028f9b6b7989d6d9e708f02cb5
Carmen Frances <carmenfrances@nem1.net>
2022-10-26 15:51:01 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Extract the domain name from a URL
+
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// domainName("http://github.com/carbonfive/raygun") == "github" 
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet
+
+
+//HINT https://www.codewars.com/kata/514a024011ea4fb54200004b
+
+//To extract hostname from string
+function domainName(url){
+    url = url.replace("https://", '');
+    url = url.replace("http://", '');
+    url = url.replace("www.", '');
+    return url.split('.')[0];
+  };
+  
+  console.log(domainName("https://gocart.com"))
